aa33e2c2501efe97c9a6c52b7f3b62b9f6c1b488
Marina Marinova <marinavmarinova@gmail.com>
2025-01-04 15:38:57 +0200

    Ex 1 complete

---
+++ src/components/Guess/Guess.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Guess() {
+  const [guess, setGuess] = React.useState('');
+
+  const submitGuess = (event) => {
+    event.preventDefault();
+
+    console.log(guess);
+
+    setGuess('');
+  };
+  return (
+    <form className="guess-input-wrapper" onSubmit={submitGuess}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        title="A five-letter word"
+        minLength="5"
+        maxLength="5"
+        pattern="[A-Z]{5,5}"
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={(event) => setGuess(event.target.value.toUpperCase())}
+      />
+    </form>
+  );
+}
+
+export default Guess;
